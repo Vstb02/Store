3ee5858795e363a29ba7c0f4fe88ffacc7ad9028
Vstb02 <v.steblev02@gmail.com>
2022-12-24 23:51:35 +0300

    Сервис генерации jwt

---
+++ src/Core/Store.Application/Interfaces/ITokenClaimsService.cs
@@ -0,0 +1,9 @@
+﻿using Store.Domain.Identity;
+
+namespace Store.Application.Interfaces
+{
+    public interface ITokenClaimsService
+    {
+        Task<string> GetTokenAsync(ApplicationUser user);
+    }
+}
---
+++ src/Core/Store.Application/Services/TokenClaimsService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Store.Application.Interfaces;
+using Store.Domain.Identity;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Store.Application.Services
+{
+    public class TokenClaimsService : ITokenClaimsService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public TokenClaimsService(UserManager<ApplicationUser> userManager,
+                                  IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        public async Task<string> GetTokenAsync(ApplicationUser user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration.GetSection("JWT:SecurityKey").Value);
+            var roles = await _userManager.GetRolesAsync(user);
+            var claims = new List<Claim> { new Claim(ClaimTypes.Name, user.UserName) };
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(claims.ToArray()),
+                Expires = DateTime.UtcNow.AddDays(Convert.ToDouble(_configuration.GetSection("JWT:LifeTime").Value)),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
