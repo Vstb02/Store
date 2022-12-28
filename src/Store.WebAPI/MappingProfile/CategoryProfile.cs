b48641df746f38334ad3b65ceaf29a0aa0018404
Vstb02 <v.steblev02@gmail.com>
2022-12-28 11:42:00 +0300

    Маппинг моделей

---
+++ src/Store.WebAPI/MappingProfile/CategoryProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Store.Domain.Entities;
+using Store.WebAPI.Models.Categories;
+
+namespace Store.WebAPI.MappingProfile
+{
+    public class CategoryProfile : Profile 
+    {
+        public CategoryProfile()
+        {
+            CreateMap<CreateCategoryDto, ProductCategory>();
+
+            CreateMap<CategoryDto, ProductCategory>().ReverseMap();
+
+            CreateMap<UpdateCategoryDto, ProductCategory>();
+        }
+    }
+}
---
+++ src/Store.WebAPI/MappingProfile/ProductProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Store.Domain.Entities;
+using Store.WebAPI.Models.Products;
+
+namespace Store.WebAPI.MappingProfile
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<ProductImage, ProductImageDto>().ReverseMap();
+
+            CreateMap<CreateProductDto, Product>()
+                .ForMember(x => x.ProductImages, opt => opt.MapFrom(src => src.ProductImages));
+
+            CreateMap<ProductImageDto, ProductImage>();
+
+            CreateMap<UpdateProductDto, Product>()
+                .ForMember(x => x.ProductImages, opt => opt.MapFrom(src => src.ProductImages));
+
+            CreateMap<ProductDto, Product>()
+                .ForMember(x => x.ProductImages, opt => opt.MapFrom(src => src.ProductImages));
+
+            CreateMap<Product, ProductDto>()
+                .ForMember(x => x.ProductImages, opt => opt.MapFrom(src => src.ProductImages));
+        }
+    }
+}
