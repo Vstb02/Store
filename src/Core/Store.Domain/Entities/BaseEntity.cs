1377e69361ceac5fb266fac686037dabffe9069f
Vstb02 <v.steblev02@gmail.com>
2022-12-28 17:03:31 +0300

    Убрал update значение по умолчанию

--- src/Core/Store.Domain/Entities/BaseEntity.cs
+++ src/Core/Store.Domain/Entities/BaseEntity.cs
@@ -4,6 +4,6 @@
     {
         public T Id { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
-        public DateTime Updated { get; set; } = DateTime.Now;
+        public DateTime Updated { get; set; }
     }
 }
