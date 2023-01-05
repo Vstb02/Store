b53436faa1cec707d2f9c975b699b0a943fde353
Vladisalv <v.steblev02@gmail.com>
2023-01-06 01:27:39 +0300

    Заменил IEnumerable на List

--- src/Store.Infrastructure/Data/Repositories/ProductRepository.cs
+++ src/Store.Infrastructure/Data/Repositories/ProductRepository.cs
@@ -23,7 +23,7 @@ namespace Store.Infrastructure.Data.Repositories
             return entity;
         }
 
-        public override async Task<IEnumerable<Product>> GetAll(FilterPagingDto paging, CancellationToken cancellationToken = default)
+        public override async Task<List<Product>> GetAll(FilterPagingDto paging, CancellationToken cancellationToken = default)
         {
             var query = _context.Products.Include(x => x.ProductImages);
             var pagingQuery = ApplyPaging(query, paging);
