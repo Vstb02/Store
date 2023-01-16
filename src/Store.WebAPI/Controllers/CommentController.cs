29854ed858b9ad84643f7d0493a35c2e71ea0206
Vladisalv <v.steblev02@gmail.com>
2023-01-16 15:22:30 +0300

    Комментарии

--- src/Store.WebAPI/Controllers/CommentController.cs
+++ src/Store.WebAPI/Controllers/CommentController.cs
@@ -15,6 +15,16 @@ namespace Store.WebAPI.Controllers
             _commentService = commentService;
         }
 
+        /// <summary>
+        /// Добавление комментария к товару
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <param name="commentDto"></param>
+        /// <returns>CommentItemDto</returns>
+        /// <response code="200">Success</response>
+        /// <response code="400">Bad Request</response>
+        /// <response code="404">Not Found</response>
+        /// <response code="500">Internal Server Error</response>
         [HttpPost]
         public async Task<IActionResult> AddComment([FromQuery]Guid productId, CreateCommentDto commentDto)
         {
@@ -23,6 +33,15 @@ namespace Store.WebAPI.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// Удаление комментария
+        /// </summary>
+        /// <param name="commentId"></param>
+        /// <returns></returns>
+        /// <response code="200">Success</response>
+        /// <response code="400">Bad Request</response>
+        /// <response code="404">Not Found</response>
+        /// <response code="500">Internal Server Error</response>
         [HttpPost]
         public async Task<IActionResult> DeleteComment([FromQuery] Guid commentId)
         {
@@ -31,6 +50,15 @@ namespace Store.WebAPI.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// Получение списка комментариев к товару
+        /// </summary>
+        /// <param name="paging"></param>
+        /// <param name="filter"></param>
+        /// <returns>List CommentItemDto</returns>
+        /// <response code="200">Success</response>
+        /// <response code="400">Bad Request</response>
+        /// <response code="500">Internal Server Error</response>
         [HttpGet]
         public async Task<IActionResult> GetProductsComment([FromQuery] FilterPagingDto paging, [FromQuery] CommentFilterDto filter)
         {
