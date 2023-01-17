1f2b020a8aba27e781ee73cc6623ecc5e5e06e03
Vladisalv <v.steblev02@gmail.com>
2023-01-17 17:02:02 +0300

    Добавил валидацию для dto

--- src/Core/Store.Application/Models/Comments/CreateCommentDto.cs
+++ src/Core/Store.Application/Models/Comments/CreateCommentDto.cs
@@ -1,7 +1,12 @@
-﻿namespace Store.Application.Models.Comments
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Store.Application.Models.Comments
 {
     public record class CreateCommentDto
     {
+        [Display(Name = "Содержание")]
+        [Required(ErrorMessage = "Поле «{0}» не заполнено")]
+        [MaxLength(500, ErrorMessage = "Поле «{0}» превысило максимально допустимое значение в «{1}» символов")]
         public string Content { get; init; }
     }
 }
--- src/Core/Store.Application/Models/Filters/FilterPagingDto.cs
+++ src/Core/Store.Application/Models/Filters/FilterPagingDto.cs
@@ -1,8 +1,17 @@
-﻿namespace Store.Application.Models.Filters
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Store.Application.Models.Filters
 {
     public class FilterPagingDto
     {
+        [Display(Name = "Номер страницы")]
+        [Required(ErrorMessage = "Поле «{0}» не заполнено")]
+        [Range(0, 100, ErrorMessage = "Значение для {0} должно быть между {1} ​​и {2}.")]
         public int PageNumber { get; set; }
+        [Display(Name = "Размер страницы")]
+        [Required(ErrorMessage = "Поле «{0}» не заполнено")]
+        [Range(0, 100, ErrorMessage = "Значение для {0} должно быть между {1} ​​и {2}.")]
         public int PageSize { get; set; }
     }
 }
